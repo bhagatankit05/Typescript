eece7daac9e72c7148cb7b7fe3ec1cb3dc351c4c
bhagatankit05 <bhagatankit82@gmail.com>
2025-12-03 19:15:42 +0530

    Add primitives.ts with initial variable declarations and console log

---
+++ primitives.ts
@@ -0,0 +1,8 @@
+let age: number = 22;
+let username: string = "ankit";
+let active: boolean = true;
+let scores: number[] = [10, 20, 30];
+
+let userTuple: [number, string] = [1, "Ankit"];
+
+console.log(age, username, active, scores, userTuple);
